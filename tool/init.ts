00d41ed10e997d6a91e17d329e7a7d11e32ba941
Natalie Weizenbaum <nweiz@google.com>
2020-12-23 16:41:50 -0800

    Build Embedded Dart Sass from the master branch by default

--- tool/init.ts
+++ tool/init.ts
@@ -2,12 +2,27 @@
 // MIT-style license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import yargs from 'yargs';
+
 import {getDartSassEmbedded, getEmbeddedProtocol} from './embedded-binaries';
+
+const argv = yargs(process.argv.slice(2))
+  .option('release', {
+    type: 'boolean',
+    description: 'Download a released version of Embedded Dart Sass',
+  })
+  .option('embedded-host-version', {
+    type: 'string',
+    description: 'The version or ref of Embedded Dart Sass to download',
+  }).argv;
 
 async function main() {
   const outPath = 'lib/src/vendor';
   await getEmbeddedProtocol(outPath);
-  await getDartSassEmbedded(outPath);
+  await getDartSassEmbedded(outPath, {
+    release: argv.release,
+    version: argv['embedded-host-version'],
+  });
 }
 
 main();
